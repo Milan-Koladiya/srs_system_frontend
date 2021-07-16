79cf831af1fcb820403440ff9fbd17776ec734ea
Milan Koladiya <milankoladiya444@gmail.com>
2021-07-17 00:40:17 +0530

    added a component and nested component

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: "App-Server",
+    templateUrl: "./server.component.html" 
+})
+
+export class ServerComponent {
+    componentTitle = "This is App component"
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: "App-Servers",
+    // selector: ["App-Servers"],
+    // selector: '.App-Servers',
+    templateUrl: "./servers.component.html"
+})
+
+export class ServersComponent {
+    title = "This is server Component"
+}
