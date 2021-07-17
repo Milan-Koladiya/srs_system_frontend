2b68f7ac9382653ef3a52a1e0f0a550bff51638d
Milan Koladiya <milankoladiya444@gmail.com>
2021-07-17 21:49:55 +0530

    string-interpolation && property binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,10 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component ({
     selector: "App-Server",
     templateUrl: "./server.component.html" 
 })
 
-export class ServerComponent {
-    componentTitle = "This is App component"
+export class ServerComponent implements OnInit {
+    componentTitle = "This is App component";
+    propertyBindingButton = true;
+    constructor() {
+        setTimeout(() => {
+            this.propertyBindingButton = false;
+            this.componentTitle = "Title Changed";
+        }, 3000)
+    }
+
+    ngOnInit() {
+        console.log("OnInit function Calledd It is same as componentDidMount in react");
+    }
 }
