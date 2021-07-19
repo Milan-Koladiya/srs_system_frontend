c621340dba5ea5b93ea4be2d9e20b69ab12c704b
Milan Koladiya <milankoladiya444@gmail.com>
2021-07-19 21:36:19 +0530

    added a project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,24 @@ import { FormsModule }  from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipesDetailComponent } from './recipes/recipe-detail/recipes-detail.component';
+import { RecipesListComponent } from './recipes/recipe-list/recipes-list.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    HeaderComponent,
+    RecipesComponent,
+    RecipesDetailComponent,
+    RecipesListComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
